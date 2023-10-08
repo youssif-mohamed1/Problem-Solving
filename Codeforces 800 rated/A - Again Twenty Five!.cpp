66b1155aba7befc18396b16de7749977a0367a28
Youssef Mohamed Ahmed Ismail <108690456+youssif-mohamed1@users.noreply.github.com>
2023-10-08 23:17:51 +0300

    Add Codeforces 800 rated/A - Again Twenty Five!.cpp

---
+++ Codeforces 800 rated/A - Again Twenty Five!.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <cstring>
+#include <set>
+#include<iomanip>
+#include<math.h>
+#include<cmath>
+using namespace std;
+int main()
+{
+string x;
+cin>>x;
+cout<<25;
+return 0;
+}

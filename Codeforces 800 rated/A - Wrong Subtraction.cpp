7cfdf187b216515b74a1ad6e10ca76c3e60d2abc
Youssef Mohamed Ahmed Ismail <108690456+youssif-mohamed1@users.noreply.github.com>
2023-10-08 23:10:58 +0300

    Add Codeforces 800 rated/A - Wrong Subtraction.cpp

---
+++ Codeforces 800 rated/A - Wrong Subtraction.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i < k; i++)
+	{
+		if (n%10 != 0)
+		{
+			n--;
+		}
+		else if (n % 10 == 0)
+		{
+			n /= 10;
+		}
+	}
+	cout << n;
+	return 0;
+}
